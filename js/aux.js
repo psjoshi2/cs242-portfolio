160f0962dde52c9b618a9a55bfc9d3e7611ea9d2
Bryce Dorn <brycedorn@gmail.com>
2014-03-13 00:28:03 -0500

    moves aux functions into their own file

--- js/aux.js
+++ js/aux.js
@@ -9,3 +9,65 @@ function loadXMLDoc(filename) {
   xhttp.send();
   return xhttp.responseXML;
 }
+
+function get_json(path)
+{
+  var list = loadXMLDoc(path);
+  return xmlToJson(list);
+}
+
+// Struct factory, takes in params for struct and returns constructor
+function makeStruct(params) {
+  var params = params.split(',');
+  var num_params = params.length;
+  function Object() {
+    for (var i = 0; i < num_params; i++) {
+      this[params[i]] = arguments[i];
+    }
+  }
+  return Object;
+}
+
+// Changes XML to JSON - Taken from http://davidwalsh.name/convert-xml-json
+function xmlToJson(xml) {
+  
+  // Create the return object
+  var obj = {};
+
+  if (xml.nodeType == 1) { // element
+    // do attributes
+    if (xml.attributes.length > 0) {
+    obj["@attributes"] = {};
+      for (var j = 0; j < xml.attributes.length; j++) {
+        var attribute = xml.attributes.item(j);
+        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+  } else if (xml.nodeType == 3) { // text
+    obj = xml.nodeValue;
+  }
+
+  // do children
+  if (xml.hasChildNodes()) {
+    for(var i = 0; i < xml.childNodes.length; i++) {
+      var item = xml.childNodes.item(i);
+      var nodeName = item.nodeName;
+      if (typeof(obj[nodeName]) == "undefined") {
+        obj[nodeName] = xmlToJson(item);
+      } else {
+        if (typeof(obj[nodeName].push) == "undefined") {
+          var old = obj[nodeName];
+          obj[nodeName] = [];
+          obj[nodeName].push(old);
+        }
+        obj[nodeName].push(xmlToJson(item));
+      }
+    }
+  }
+  return obj;
+};
+
+// Reverses a string (useful for extracting filenames)
+function reverse(s){
+  return s.split("").reverse().join("");
+}
