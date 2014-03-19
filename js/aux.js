ecfa5904119af4c552b2133f20a0aa1ada00e28f
Bryce Dorn <brycedorn@gmail.com>
2014-03-19 00:02:57 -0500

    pushobject - nice!

--- js/aux.js
+++ js/aux.js
@@ -1,8 +1,12 @@
+// Ah.
+String.prototype.repeat = function (num) {
+  return new Array(num + 1).join(this);
+};
+
 function loadXMLDoc(filename) {
   if (window.XMLHttpRequest) {
     xhttp = new XMLHttpRequest();
-  } else // code for IE5 and IE6
-  {
+  } else { // IE5 and IE6
     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
   }
   xhttp.open("GET", filename, false);
@@ -10,8 +14,7 @@ function loadXMLDoc(filename) {
   return xhttp.responseXML;
 }
 
-function get_json(path)
-{
+function getJSON(path) {
   var list = loadXMLDoc(path);
   return xmlToJson(list);
 }
@@ -30,7 +33,6 @@ function makeStruct(params) {
 
 // Changes XML to JSON - Taken from http://davidwalsh.name/convert-xml-json
 function xmlToJson(xml) {
-  
   // Create the return object
   var obj = {};
 
@@ -87,5 +89,57 @@ function compareNames(a, b){
 
 // Dynamically load file contents into file's iframe
 function iframeIt(url,id) {
+  $("iframe#"+id).css('display','block');
   $("iframe#"+id).attr("src", url);
+}
+
+// Taken from http://stackoverflow.com/questions/8398403/jquery-javascript-to-check-if-correct-e-mail-was-entered
+function validateEmail(email) { 
+    var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+    return pattern.test(email);
+}
+
+// Profanity filter, taken from http://stackoverflow.com/questions/4541070/how-can-i-do-a-jquery-swear-word-bad-word-filter
+function filterWords(txt) {
+  var filter = ['ass', 'words'];
+  for(var i=0; i<swears.length; i++){
+    var pattern = new RegExp('\\b' + swears[i] + '\\b', 'g');
+    var replacement = '*'.repeat(swears[i].length);
+    txt = txt.replace(pattern, replacement);
+  }
+  return txt;
+}
+
+// Function to add comments
+function addComment(div) {
+  // Comment struct
+  var Comment = makeStruct("id,content,author,date,subs");
+  var subcomments = [];
+
+  var unclean = div.parent().find("textarea").val();
+  content = filterWords(unclean);
+
+  // Author is email, or default is Anonymous
+  var author = div.parent().find("input").val();
+  if(!author) author = "Anonymous";  
+
+  // Find the file in projects[] that this is commenting on (or Project that it is commenting on)
+  var date = $.format.date(new Date().getTime(),"MMM dd yyyy, h:mma");
+  var proj_id = div.parent().attr('id');
+  var file_id = div.attr('id');
+
+  // Gravatar - if we have time
+  // var gravatar = $.gravatar(validateEmail(author));
+  // console.log("GRAV: "+gravatar);
+
+  if(file_id) { // File comment
+    var comment_id = projects[proj_id].files[file_id].comments.length;
+    var new_comment = new Comment(comment_id,content,author,date,subcomments);
+    projects[proj_id].files[file_id].comments.pushObject(new_comment);
+  }
+  else { // Project comment
+    var comment_id = projects[proj_id].comments.length;
+    var new_comment = new Comment(comment_id,content,author,date,subcomments);
+    projects[proj_id].comments.pushObject(new_comment);
+  }
 }
