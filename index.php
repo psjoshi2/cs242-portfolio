fb15f493786d2e3090e141ae02e5d060c0667966
Bryce Dorn <brycedorn@gmail.com>
2014-04-02 16:45:54 -0500

    adds call to get data asynchronously

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php include_once("index.html"); ?>
+<?php
+include_once("index.html");
+$interval=60; //minutes
+set_time_limit(0);
+while (true)
+{
+  $now=time();
+  include("svn.php");
+  sleep($interval*60-(time()-$now));
+}
+?>
