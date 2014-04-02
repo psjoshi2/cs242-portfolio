ff35fc329dfb7af1c2e3aa3f326b9e33a68793dd
Bryce Dorn <brycedorn@gmail.com>
2014-04-02 16:46:19 -0500

    shell command to retrieve data

--- php/svn.php
+++ php/svn.php
@@ -1,1 +1,3 @@
-<!-- echo shell_exec('svn list --xml --recursive https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > svn_list.xml'); -->
+<?php
+exec('svn list --xml --recursive https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_list.xml'); exec('svn log --verbose --xml https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_log.xml');
+?>
