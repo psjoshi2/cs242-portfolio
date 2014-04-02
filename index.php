b49fb89553385d36e2fcbda33718b9b7ec541c35
Bryce Dorn <brycedorn@gmail.com>
2014-04-02 18:29:47 -0500

    re-adds svn call

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+exec('svn list --xml --recursive https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_list.xml'); exec('svn log --verbose --xml https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_log.xml');
 include_once("index.html");
 $interval=60; //minutes
 set_time_limit(0);
