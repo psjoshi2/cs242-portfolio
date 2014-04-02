e1549436b049df87b5893ffec53e1e36ccddaff4
Bryce Dorn <brycedorn@gmail.com>
2014-04-02 16:48:03 -0500

    deprecated

--- js/svn.js
+++
@@ -1,9 +0,0 @@
-// $.ajax({
-//   url : 'php/svn.php'
-// }).done(function(data) {
-//   console.log(data);
-// });
-
-// svn log --verbose --xml https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_log.xml
-
-// svn list --xml --recursive https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_list.xml
