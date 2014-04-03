26077bb7a84936f34e4b455b1d07063527b03a91
Bryce Dorn <brycedorn@gmail.com>
2014-04-03 17:33:57 -0500

    adds script to fetch data every hour

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
 <?php
-// exec('svn list --xml --recursive https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_list.xml'); exec('svn log --verbose --xml https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_log.xml');
-// echo("<script>console.log('Hello World!');</script>");
-echo "Should echo regardless.";
-
+exec('svn list --xml --recursive https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_list.xml'); exec('svn log --verbose --xml https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_log.xml');
 include_once("index.html");
-if ($_GET['run']) {
-  # This code will run if ?run=true is set.
-  echo "Button was clicked.";  
+$interval=60; //minutes
+set_time_limit(0);
+while (true)
+{
+  $now=time();
+  include("php/svn.php");
+  sleep($interval*60-(time()-$now));
 }
-// $interval=60; //minutes
-// set_time_limit(0);
-// while (true)
-// {
-//   $now=time();
-//   include("php/svn.php");
-//   sleep($interval*60-(time()-$now));
-// }
-// if($_GET['run']){
-//   echo("<script>console.log('Hello World!');</script>");
-// }
 ?>
