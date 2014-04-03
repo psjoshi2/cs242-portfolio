1f629eadb93d7b1fed499565e6702bfb33617ae4
Bryce Dorn <brycedorn@gmail.com>
2014-04-03 17:34:22 -0500

    simplifies script for fetching svn data

--- php/svn.php
+++ php/svn.php
@@ -1,3 +1,5 @@
 <?php
+echo("Updating DB...");
 exec('svn list --xml --recursive https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_list.xml'); exec('svn log --verbose --xml https://subversion.ews.illinois.edu/svn/sp14-cs242/bsdorn2 > data/svn_log.xml');
+echo("done!");
 ?>
